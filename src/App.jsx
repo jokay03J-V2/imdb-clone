1fb9922a5910e2fcdc850dd91a2b44ed9e7ef4a5
jokay03J-V2 <jmagro@edenschool.fr>
2022-12-13 17:14:31 +0100

    update router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import HomePage from './pages/Home'
-import ErrorPage from './pages/Error'
+import ErrorPage from './pages/NotFound'
 import SearchPage from './pages/Search'
+import ErrorBoundary from './pages/ErrorBoundary'
 function App() {
 
-  const routes = createBrowserRouter([{ path: "/imdb-clone/", element: <HomePage></HomePage>}, { path: "/imdb-clone/search", element: <SearchPage></SearchPage>}, { path: "*", element: <ErrorPage></ErrorPage>}])
+  const routes = createBrowserRouter([
+    { path: "/imdb-clone/", element: <HomePage></HomePage>, errorElement: <ErrorBoundary></ErrorBoundary> },
+    { path: "/imdb-clone/search", element: <SearchPage></SearchPage>, errorElement: <ErrorBoundary></ErrorBoundary> },
+    { path: "*", element: <ErrorPage></ErrorPage>, errorElement: <ErrorBoundary></ErrorBoundary> }
+  ]);
 
-  return <div className='h-screen w-screen'><RouterProvider router={routes}></RouterProvider></div>
+  return <div className='h-screen w-full'><RouterProvider router={routes}></RouterProvider></div>
 }
 
 export default App
