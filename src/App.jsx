82dcde549026b27d7e5b9f3ce6283211a9289e05
jokay03J-V2 <jmagro@edenschool.fr>
2022-12-13 15:03:00 +0100

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import './App.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import HomePage from './pages/Home'
 import ErrorPage from './pages/Error'
-import { Provider } from 'react-redux'
-import { store } from "./store"
 import SearchPage from './pages/Search'
 function App() {
 
-  const routes = createBrowserRouter([{ path: "", element: <HomePage></HomePage>}, { path: "/search", element: <SearchPage></SearchPage>}, { path: "*", element: <ErrorPage></ErrorPage>}])
+  const routes = createBrowserRouter([{ path: "/imdb-clone/", element: <HomePage></HomePage>}, { path: "/imdb-clone/search", element: <SearchPage></SearchPage>}, { path: "*", element: <ErrorPage></ErrorPage>}])
 
   return <div className='h-screen w-screen'><RouterProvider router={routes}></RouterProvider></div>
 }
